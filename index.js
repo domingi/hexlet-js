47cb0378d92b7d640bde60ce2fd71ff307b9bd18
Станислав Грибанов <youkillbird@gmail.com>
2022-12-29 16:48:08 +0500

    Add eslint

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import _ from "lodash";
-console.log("Hello, Hexlet!");
+import _ from 'lodash';
 
-console.log(_.last(["one", "two"]));
+console.log('Hello, Hexlet!');
+
+console.log(_.last(['one', 'two']));
